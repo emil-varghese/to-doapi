2d8ea717cd12f4ae22b729f065f5b25c5cc4e091
Emil Varghese <emilvpulinattu@gmail.com>
2016-12-02 15:07:01 -0800

    Added PUT to Heroku

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.get('/todos', function(req, res) {
 			$like: '%' + query.q.trim() + '%'
 		};
 	}
-	console.log(where);
 
 	db.todo.findAll({
 		where: where
@@ -99,7 +98,7 @@ app.delete('/todos/:id', function(req, res) {
 				"error": "Not found with id"
 			});
 		} else {
-			res.status(200).json({
+			res.status(204).json({
 				"success": rowsDeleted + " items deleted"
 			});
 		}
@@ -113,31 +112,30 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
-
-	res.json(matchedTodo);
+	db.todo.findById(todoId).then (function (todo) {
+		if (todo) {
+			return todo.update(attributes)
+		} else {
+			res.status(404).send();
+		}
+	}, function () {
+		res.status(500).send();
+	}).then (function (todo) {
+		res.json(todo.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	})
 
 });
 
